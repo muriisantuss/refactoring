2d6a1134e43ee5f408cc8df95501f278f1eec34f
muriisantuss <muriisantuss@gmail.com>
2024-09-21 15:00:13 -0300

    :sparkles: feat: create Node class for linked list

---
+++ src/linearlist/Node.java
@@ -0,0 +1,42 @@
+package linearlist;
+
+public class Node<T> {
+  private T element;
+  private Node<T> next;
+
+  public Node(){
+    this.element = null;
+    this.next = null;
+  }
+  
+  public Node(T element){
+    this.element = element;
+    this.next = null;
+  }
+
+  public Node(T element, Node<T> next){
+    this.element = element;
+    this.next = next;
+  }
+  
+  public T getElement() {
+    return element;
+  }
+
+  public void setElement(T element) {
+    this.element = element;
+  }
+
+  public Node<T> getNext() {
+    return next;
+  }
+
+  public void setNext(Node<T> next) {
+    this.next = next;
+  }
+
+
+
+
+
+}
---
+++ src/linearlist/Run.java
@@ -0,0 +1,7 @@
+package linearlist;
+
+public class Run {
+  public static void main(String[] args) {
+    
+  }
+}
