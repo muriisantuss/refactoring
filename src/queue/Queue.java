72ce30c4de7f5dedf95ae88383ad6e4f705127e3
muriisantuss <muriisantuss@gmail.com>
2024-10-15 22:07:40 -0300

    :sparkles: feat: Implements the 'isEmpty' and 'isFull' methods in the queue project

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -4,4 +4,11 @@ public class Queue extends Node{
     public Queue(int size) {
         super(size);
     }
+
+    public boolean isEmpty(){
+        return (getTotal() == 0);
+    }
+    public boolean isFull(){
+        return (getTotal() == getValues().length);
+    }
 }
